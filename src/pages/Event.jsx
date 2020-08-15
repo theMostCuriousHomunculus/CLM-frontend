68aaf80bd1b1eeac1ba9abdc636bb7cdb4392a85
Casey Johnson <johnson.casey1211@gmail.com>
2020-08-15 19:47:10 -0400

    another issue with the event page

--- src/pages/Event.jsx
+++ src/pages/Event.jsx
@@ -223,7 +223,7 @@ const Event = () => {
                               <CSVLink
                                 className={classes.downloadLink}
                                 data={plr.card_pool.reduce(function (a, c) {
-                                  return c && c.mtgo_id ? a + " ,1," + c + ", , , , \n" : a;
+                                  return c && c.mtgo_id ? a + " ,1," + c.mtgo_id + ", , , , \n" : a;
                                 }, "Card Name,Quantity,ID #,Rarity,Set,Collector #,Premium\n")}
                                 filename={`${eventState.name + " - " + plr.name}.csv`}
                                 target="_blank"
