83a54762c20ba191a67423ebabcdf7dd605ded94
Casey Johnson <johnson.casey1211@gmail.com>
2021-05-23 21:57:46 -0500

    hammering out event page graphql refactor

--- src/pages/Event.jsx
+++ src/pages/Event.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import arrayMove from 'array-move';
-import io from 'socket.io-client';
 import MUICard from '@material-ui/core/Card';
 import MUICardContent from '@material-ui/core/CardContent';
 import MUICardHeader from '@material-ui/core/CardHeader';
@@ -10,6 +8,7 @@ import MUITab from '@material-ui/core/Tab';
 import MUITabs from '@material-ui/core/Tabs';
 import MUITooltip from '@material-ui/core/Tooltip';
 import MUITypography from '@material-ui/core/Typography';
+import { createClient } from 'graphql-ws';
 import { CSVLink } from 'react-csv';
 import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
@@ -21,19 +20,7 @@ import SmallAvatar from '../components/miscellaneous/SmallAvatar';
 import SortableList from '../components/Event Page/SortableList';
 import theme from '../theme';
 import { AuthenticationContext } from '../contexts/authentication-context';
-import { useRequest } from '../hooks/request-hook';
-
-const eventReducer = (state, action) => {
-  switch (action.type) {
-    case 'UPDATE_EVENT':
-      return {
-        ...state,
-        ...action.value
-      };
-    default:
-      return state;
-  }
-};
+import { fetchEventByID, moveCard, selectCard, sortCard } from '../requests/GraphQL/event-requests.js';
 
 const useStyles = makeStyles({
   downloadLink: {
@@ -52,268 +39,278 @@ const useStyles = makeStyles({
 const Event = () => {
 
   const authentication = React.useContext(AuthenticationContext);
-  const { sendRequest } = useRequest();
   const classes = useStyles();
-  const eventId = useParams().eventId;
+  const eventID = useParams().eventId;
   const [dialogDisplayed, setDialogDisplayed] = React.useState(false);
   const [errorMessage, setErrorMessage] = React.useState();
-  // this needs to be removed; should not send an http request for this, just send back this info using websockets
-  const [playerUsername, setPlayerUsername] = React.useState();
+  const [event, setEvent] = React.useState({
+    finished: false,
+    host: {},
+    name: '',
+    players: []
+  });
   const [selectedCard, setSelectedCard] = React.useState();
-  const [socket, setSocket] = React.useState();
   const [tabNumber, setTabNumber] = React.useState(0);
 
-  const [eventState, dispatch] = React.useReducer(eventReducer, {
-    chaff: [],
-    finished: false,
-    mainboard: [],
-    name: '',
-    other_players_card_pools: [],
-    pack: [],
-    players: [],
-    sideboard: []
-  });
-
   React.useEffect(function () {
-    async function fetchData () {
-      try {
-        const accountData = await sendRequest(
-          `${process.env.REACT_APP_REST_URL}/account/${authentication.userId}`,
-          'GET',
-          null,
-          { Authorization: 'Bearer ' + authentication.token }
-        );
-        setPlayerUsername(accountData.name);
-      } catch (error) {
-        console.log(error);
+    const client = createClient({
+      url: process.env.REACT_APP_GRAPHQL_WS_URL
+    });
+
+    const eventData = fetchEventByID(eventID, authentication.token);
+
+    setEvent(eventData);
+
+    async function subscribe () {
+      function onNext(update) {
+        setEvent(update.data.joinEvent);
       }
+
+      await new Promise((resolve, reject) => {
+        client.subscribe({
+          query: `subscription {
+            joinEvent(${eventID}) {
+              finished
+              name
+              players {
+                account {
+                  _id
+                  avatar
+                  name
+                }
+                chaff {
+                  _id
+                  back_image
+                  image
+                  mtgo_id
+                  name
+                }
+                current_pack {
+                  _id
+                  back_image
+                  image
+                  name
+                }
+                mainboard {
+                  _id
+                  back_image
+                  image
+                  mtgo_id
+                  name
+                }
+                sideboard {
+                  _id
+                  back_image
+                  image
+                  mtgo_id
+                  name
+                }
+              }
+            }
+          }`
+        },
+        {
+          complete: resolve,
+          error: reject,
+          next: onNext
+        });
+      })
     }
-    fetchData();
-    setErrorMessage(null);
-    setSocket(io(`${process.env.REACT_APP_REST_URL.replace('/rest', '')}`,
-      { query: { eventId, userId: authentication.userId } }));
-  }, [authentication.token, authentication.userId, eventId, sendRequest]);
+    subscribe(result => console.log(result), error => console.log(error));
+  }, [authentication.token, eventID]);
 
-  React.useEffect(function () {
-    if (socket) {
-
-      socket.emit('join');
-
-      socket.on('admittance', function (eventInfo) {
-        dispatch({ type: 'UPDATE_EVENT', value: eventInfo });
-      });
-
-      socket.on('error', function (error) {
-        setErrorMessage(error.message);
-      });
-
-      socket.on('updateEventStatus', function (eventInfo) {
-        dispatch({ type: 'UPDATE_EVENT', value: eventInfo });
-      });
-
-      return function () {
-        socket.emit('leave');
-        socket.disconnect(true);
-      }
-    }
-  }, [authentication.userId, eventId, socket]);
-
-  const usedCardsString = eventState.mainboard.reduce(function (a, c) {
+  const me = event.players.find(plr => plr.account._id === authentication.userId);
+  const others = event.players.filter(plr => plr.account._id !== authentication.userId);
+  const usedCardsString = me.mainboard.reduce(function (a, c) {
     return c && c.mtgo_id ? `${a}"${c.name}",1,${c.mtgo_id}, , , , ,No,0\n` : a;
   }, "");
-  const unUsedCardsString = eventState.chaff.concat(eventState.sideboard).reduce(function (a, c) {
+  const unUsedCardsString = me.chaff.concat(me.sideboard).reduce(function (a, c) {
     return c && c.mtgo_id ? `${a}"${c.name}",1,${c.mtgo_id}, , , , ,Yes,0\n` : a;
   }, "");
 
-  function moveCard (cardId, fromCards, toCards) {
-    const fromCardsClone = [...eventState[fromCards]];
-    const indexOfCard = fromCardsClone.findIndex(function (card) {
-      return card._id === cardId;
-    });
-    const cardToMove = fromCardsClone.splice(indexOfCard, 1);
-    const toCardsClone = [...eventState[toCards]].concat(cardToMove);
-
-    dispatch({
-      type: 'UPDATE_EVENT',
-      value: {
-        [fromCards]: fromCardsClone,
-        [toCards]: toCardsClone
-      }
-    });
-
-    socket.emit('moveCard', cardId, fromCards, toCards);
+  async function onMoveCard (cardID, destination, origin) {
+    try {
+      const eventData = await moveCard(cardID, destination, eventID, origin, authentication.token);
+      
+      setEvent(eventData);
+    } catch (error) {
+      setErrorMessage(error.message);
+    }
   }
 
-  function onSortEnd ({ collection, newIndex, oldIndex }) {
-    dispatch({
-      type: 'UPDATE_EVENT',
-      value: {
-        [collection]: arrayMove(eventState[collection], oldIndex, newIndex)
-      }
-    });
+  async function onSelectCard (cardID) {
+    try {
+      const eventData = await selectCard(cardID, eventID, authentication.token);
 
-    if (collection !== 'pack') {
-      socket.emit('sortCard', collection, newIndex, oldIndex);
+      setEvent(eventData);
+    } catch (error) {
+      setErrorMessage(error.message);
     }
-  };
+  }
+
+  async function onSortEnd ({ collection, newIndex, oldIndex }) {
+    try {
+      const eventData = await sortCard(collection, eventID, newIndex, oldIndex, authentication.token);
+      
+      setEvent(eventData);
+    } catch (error) {
+      setErrorMessage(error.message);
+    }
+  }
 
   return (
     <React.Fragment>
-      {socket &&
-        <React.Fragment>
-          <SelectConfirmationDialog
-            card={selectedCard}
-            open={dialogDisplayed}
-            selectCardHandler={(cardId) => socket.emit('selectCard', cardId)}
-            toggleOpen={() => setDialogDisplayed(false)}
-          />
+      <SelectConfirmationDialog
+        card={selectedCard}
+        open={dialogDisplayed}
+        selectCardHandler={(cardID) => onSelectCard(cardID)}
+        toggleOpen={() => setDialogDisplayed(false)}
+      />
 
-          <MUICard>
-            <MUICardHeader
-              disableTypography={true}
-              title={<MUITypography variant="h5">{eventState.name}</MUITypography>}
-            />
-            <MUICardContent>
-              <MUIGrid container justify="space-around" spacing={2}>
-                {eventState.players.map(function (player) {
-                  return (
-                    <MUIGrid
-                      container
-                      item
-                      justify="center"
-                      key={player.account._id}
-                      xs={6}
-                      sm={4}
-                      md={3}
-                      lg={2}
-                      xl={1}
-                    >
-                      <MUITooltip title={player.account.name}>
-                        <Link to={`/account/${player.account._id}`}>
-                          <SmallAvatar
-                            alt={player.account.name}
-                            src={player.account.avatar}
-                          />
-                        </Link>
-                      </MUITooltip>
-                    </MUIGrid>
-                  );
-                })}
-              </MUIGrid>
-            </MUICardContent>
-          </MUICard>
-
-          {// displays if the event is a draft and is not yet finished
-            !eventState.finished &&
-            <React.Fragment>
-              <MUIPaper className={classes.paper}>
-                <MUITabs
-                  className={classes.tabs}
-                  indicatorColor="primary"
-                  onChange={(event, newTabNumber) => setTabNumber(newTabNumber)}
-                  textColor="primary"
-                  value={tabNumber}
-                  variant="fullWidth"
+      <MUICard>
+        <MUICardHeader
+          disableTypography={true}
+          title={<MUITypography variant="h5">{event.name}</MUITypography>}
+        />
+        <MUICardContent>
+          <MUIGrid container justify="space-around" spacing={2}>
+            {event.players.map(function (player) {
+              return (
+                <MUIGrid
+                  container
+                  item
+                  justify="center"
+                  key={player.account._id}
+                  xs={6}
+                  sm={4}
+                  md={3}
+                  lg={2}
+                  xl={1}
                 >
-                  <MUITab label="Current Pack" />
-                  <MUITab label="My Picks" />
-                </MUITabs>
-              </MUIPaper>
+                  <MUITooltip title={player.account.name}>
+                    <Link to={`/account/${player.account._id}`}>
+                      <SmallAvatar
+                        alt={player.account.name}
+                        src={player.account.avatar}
+                      />
+                    </Link>
+                  </MUITooltip>
+                </MUIGrid>
+              );
+            })}
+          </MUIGrid>
+        </MUICardContent>
+      </MUICard>
 
-              {tabNumber === 0 &&
-                eventState.pack.length > 0 &&
-                <MUICard>
-                  <MUICardHeader
-                    disableTypography={true}
-                    title={<MUITypography variant="subtitle1">Select a Card to Draft</MUITypography>}
-                  />
-                  <SortableList
-                    axis="xy"
-                    cards={eventState.pack}
-                    clickFunction={(cardData) => {
-                      setSelectedCard(cardData);
-                      setDialogDisplayed(true);
-                    }}
-                    fromCollection="pack"
-                    moveCard={() => null}
-                    onSortEnd={onSortEnd}
-                    toCollection1={null}
-                    toCollection2={null}
-                  />
-                </MUICard>
-              }
+      {// displays if the event is a draft and is not yet finished
+        !event.finished &&
+        <React.Fragment>
+          <MUIPaper className={classes.paper}>
+            <MUITabs
+              className={classes.tabs}
+              indicatorColor="primary"
+              onChange={(event, newTabNumber) => setTabNumber(newTabNumber)}
+              textColor="primary"
+              value={tabNumber}
+              variant="fullWidth"
+            >
+              <MUITab label="Current Pack" />
+              <MUITab label="My Picks" />
+            </MUITabs>
+          </MUIPaper>
 
-              {tabNumber === 0 &&
-                eventState.pack.length === 0 &&
-                <MUICard>
-                <MUICardHeader
-                  disableTypography={true}
-                  title={<MUITypography variant="h5">Other drafters are still making their picks...</MUITypography>}
-                />
-                  <MUICardContent>
-                    <MUITypography variant="body1">
-                      Yell at them to hurry up!
-                    </MUITypography>
-                    <MUITypography variant="body1">
-                      While you're waiting, review the picks you've already made.
-                    </MUITypography>
-                  </MUICardContent>
-                </MUICard>
-              }
-
-              {tabNumber === 1 &&
-                <PicksDisplay
-                  eventState={eventState}
-                  moveCard={moveCard}
-                  onSortEnd={onSortEnd}
-                />
-              }
-            </React.Fragment>
+          {tabNumber === 0 &&
+            me.current_pack &&
+            <MUICard>
+              <MUICardHeader
+                disableTypography={true}
+                title={<MUITypography variant="subtitle1">Select a Card to Draft</MUITypography>}
+              />
+              <SortableList
+                axis="xy"
+                cards={me.current_pack}
+                clickFunction={(cardData) => {
+                  setSelectedCard(cardData);
+                  setDialogDisplayed(true);
+                }}
+                fromCollection="current_pack"
+                moveCard={() => null}
+                onSortEnd={onSortEnd}
+                toCollection1={null}
+                toCollection2={null}
+              />
+            </MUICard>
           }
 
-          {// displays once the event is finished
-            eventState.finished &&
-            <React.Fragment>
+          {tabNumber === 0 &&
+            !me.current_pack &&
+            <MUICard>
+            <MUICardHeader
+              disableTypography={true}
+              title={<MUITypography variant="h5">Other drafters are still making their picks...</MUITypography>}
+            />
+              <MUICardContent>
+                <MUITypography variant="body1">
+                  Yell at them to hurry up!
+                </MUITypography>
+                <MUITypography variant="body1">
+                  While you're waiting, review the picks you've already made.
+                </MUITypography>
+              </MUICardContent>
+            </MUICard>
+          }
+
+          {tabNumber === 1 &&
+            <PicksDisplay
+              eventState={event}
+              moveCard={onMoveCard}
+              onSortEnd={onSortEnd}
+            />
+          }
+        </React.Fragment>
+      }
+
+      {// displays once the event is finished
+        event.finished &&
+        <React.Fragment>
+          <MUITypography variant="body1">
+            <CSVLink
+              className={classes.downloadLink}
+              data={`Card Name,Quantity,ID #,Rarity,Set,Collector #,Premium,Sideboarded,Annotation\n${usedCardsString}${unUsedCardsString}`}
+              filename={`${event.name} - ${me.account.name}.csv`}
+              target="_blank"
+            >
+              Download your card pool in CSV format for MTGO play!
+            </CSVLink>
+          </MUITypography>
+          {others.map(function (plr) {
+            return (
               <MUITypography variant="body1">
                 <CSVLink
                   className={classes.downloadLink}
-                  data={`Card Name,Quantity,ID #,Rarity,Set,Collector #,Premium,Sideboarded,Annotation\n${usedCardsString}${unUsedCardsString}`}
-                  filename={`${eventState.name} - ${playerUsername}.csv`}
+                  data={plr.card_pool.reduce(function (a, c) {
+                    return a + " ,1," + c + ", , , , ,No,0\n";
+                  }, "Card Name,Quantity,ID #,Rarity,Set,Collector #,Premium,Sideboarded,Annotation\n")}
+                  filename={`${event.name} - ${plr.account.name}.csv`}
+                  key={plr.account._id}
                   target="_blank"
                 >
-                  Download your card pool in CSV format for MTGO play!
+                  Download {plr.account.name}'s card pool in CSV format for MTGO play!
                 </CSVLink>
               </MUITypography>
-              {eventState.other_players_card_pools.map(function (plr) {
-                return (
-                  <MUITypography variant="body1">
-                    <CSVLink
-                      className={classes.downloadLink}
-                      data={plr.card_pool.reduce(function (a, c) {
-                        return a + " ,1," + c + ", , , , ,No,0\n";
-                      }, "Card Name,Quantity,ID #,Rarity,Set,Collector #,Premium,Sideboarded,Annotation\n")}
-                      filename={`${eventState.name} - ${plr.account.name}.csv`}
-                      key={plr.account._id}
-                      target="_blank"
-                    >
-                      Download {plr.account.name}'s card pool in CSV format for MTGO play!
-                    </CSVLink>
-                  </MUITypography>
-                );
-              })}
+            );
+          })}
 
-              <PicksDisplay
-                eventState={eventState}
-                moveCard={moveCard}
-                onSortEnd={onSortEnd}
-              />
-            </React.Fragment>
-          }
-
-          {errorMessage &&
-            <MUITypography variant="body1">{errorMessage}</MUITypography>
-          }
+          <PicksDisplay
+            eventState={event}
+            moveCard={onMoveCard}
+            onSortEnd={onSortEnd}
+          />
         </React.Fragment>
+      }
+
+      {errorMessage &&
+        <MUITypography variant="body1">{errorMessage}</MUITypography>
       }
     </React.Fragment>
   );
--- src/requests/GraphQL/event-requests.js
+++ src/requests/GraphQL/event-requests.js
@@ -8,6 +8,120 @@ async function createEvent () {
   }
 }
 
+async function fetchEventByID (eventId, token) {
+  try {
+    const graphqlQuery = {
+      query: `
+        query {
+          fetchEventByID(_id: "${eventId}") {
+            finished
+            host {
+              _id
+            }
+            name
+            players {
+              account {
+                _id
+                avatar
+                name
+              }
+              chaff
+              current_pack
+              mainboard
+              sideboard
+            }
+      `
+    };
+    const eventData = await axios.post(process.env.REACT_APP_GRAPHQL_HTTP_URL,
+      graphqlQuery,
+      { headers: { Authorization: `Bearer ${token}` } });
+
+    return eventData.data.data.fetchEventByID;
+  } catch (error) {
+    throw new Error(error.response.data.message);
+  }
+}
+
+async function moveCard (cardID, destination, eventID, origin, token) {
+  try {
+    const graphqlQuery = {
+      query: `
+        mutation {
+          moveCard(
+            input: {
+              cardID: ${cardID}
+              destination: ${destination}
+              eventID: ${eventID}
+              origin: ${origin}
+            }
+          )
+        }
+      `
+    };
+    const eventData = await axios.post(process.env.REACT_APP_GRAPHQL_HTTP_URL,
+      graphqlQuery,
+      { headers: { Authorization: `Bearer ${token}` } });
+
+    return eventData.data.data.moveCard;
+  } catch (error) {
+    throw new Error(error.response.data.message);
+  }
+}
+
+async function selectCard (cardID, eventID, token) {
+  try {
+    const graphqlQuery = {
+      query: `
+        mutation {
+          selectCard(
+            input: {
+              cardID: ${cardID}
+              eventID: ${eventID}
+            }
+          )
+        }
+      `
+    };
+    const eventData = await axios.post(process.env.REACT_APP_GRAPHQL_HTTP_URL,
+      graphqlQuery,
+      { headers: { Authorization: `Bearer ${token}` } });
+
+    return eventData.data.data.selectCard;
+  } catch (error) {
+    throw new Error(error.response.data.message);
+  }
+}
+
+async function sortCard (collection, eventID, newIndex, oldIndex, token) {
+  try {
+    const graphqlQuery = {
+      query: `
+        mutation {
+          sortCard(
+            input: {
+              collection: ${collection}
+              eventID: ${eventID}
+              newIndex: ${newIndex}
+              oldIndex: ${oldIndex}
+            }
+          )
+        }
+      `
+    };
+    const eventData = await axios.post(process.env.REACT_APP_GRAPHQL_HTTP_URL,
+      graphqlQuery,
+      { headers: { Authorization: `Bearer ${token}` } });
+
+    return eventData.data.data.sortCard;
+  } catch (error) {
+    throw new Error(error.response.data.message);
+  }
+}
+
 export {
-  createEvent
+  createEvent,
+  fetchEventByID,
+  moveCard,
+  selectCard,
+  sortCard
 };
